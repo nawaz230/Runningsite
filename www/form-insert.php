2f5b7b071139e1350bba9852f0606a19028cf1f5
Ted <ted-varela1999@hotmail.com>
2019-07-11 11:34:36 +0200

    Modification of the form

--- www/form-insert.php
+++ www/form-insert.php
@@ -1,62 +1,53 @@
+<?php
+    //Import de la database
+    require_once("database.php");
 
+    //Création de la connexion
+    $database = new Database ();
+?>
 
 <html>
-    <head>
-  <title>
+<header>
+    <link rel="stylesheet" href="style.css">
+</header>
 
-  </title>
-    </head>
+<body>
+    <!--Title of the form-->
+    <h1>Add your activity!</h1>
 
-    <body>
     <!--here is forme begin -->
-        <form action="index.php" method="post">
+    <form action="index.html" method="post">
 
-        <label for ="title" value="title" >Title :</label>
+        <label for ="nameActivity">Title :</label>
+        <input type="text" id="nameActivity" name="title" placeholder="Insert title"><br><br>
 
-        <input type="text" id="title" name="title" placeholder="Insert title"><br><br>
+        <label for ="nameAuthor">Name :</label>
+        <input type="text" id="nameAuthor" name="name" placeholder="Insert our name"><br><br>
 
-        <label for ="Name" value="Name" >Name :</label>
-
-        <input type="text" id="name" name="name" placeholder="Name"><br><br>
-
-        <label for ="image" value="picture" >Upload Map :</label>
-
-        <input type="file" id="img" name="img"><br><br>
-
-
-        <label for ="City" value="city" >City :</label>
-
-        <input type="text" id="city" name="city" placeholder="City"><br><br>
-
-        <label for ="Postalcode" value="code" >Postal code :</label>
-
-        <input type="text" id="Postal" name="postal" placeholder="Insert Postal code"><br><br>
+        <label for ="imgActivity">Upload Map :</label>
+        <input type="file" id="imgActivity" name="img"><br><br>
 
         <label for ="Startingpoint" value="start" >Starting point :</label>
-
-        <input type="text" id="Start" name="point" placeholder="Insert Starting point"><br><br>
+        <input type="text" id="Start" name="point" placeholder="Insert starting point"><br><br>
 
         <label for ="Endingpoint" value="end" >Ending point :</label>
+        <input type="text" id="End" name="point" placeholder="Insert ending point"><br><br>
 
-        <input type="text" id="End" name="point" placeholder="Insert Ending point"><br><br>
+        <label for ="Postalcode" value="code" >Postal code :</label>
+        <input type="text" id="Postal" name="postal" placeholder="Insert postal code"><br><br>
+
+        <label for ="city">City :</label>
+        <input type="text" id="city" name="city" placeholder="Insert city"><br><br>
 
         <label for ="country" value="country" >Country :</label>
+        <input type="text" id="country" name="country" placeholder="Insert country"><br><br>
 
-        <input type="text" id="country" name="country" placeholder="Insert Country Name"><br><br>
-
-        <label for ="City" value="city" >City :</label>
-
-        <input type="text" id="city" name="city" placeholder="City"><br><br>
-
-    
-        <label for ="Discription" value="discription">Description : </label> <br>
-        
-        <textarea name="Discription" rows="5" cols="40" ></textarea><br>
+        <label for ="description">Description :</label> <br>       
+        <textarea name="description" rows="5" cols="40" ></textarea><br>
 
         <input type="submit" value="Submit">
 
-
-        </form>
+      </form>
 
 </body>
 
